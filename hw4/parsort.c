105780870d57202ceacb345dc9bbd1af76fcd546
RyadT <rtaleb1@jhu.edu>
2022-11-09 22:52:59 -0500

    gradescope version

--- hw4/parsort.c
+++ hw4/parsort.c
@@ -12,8 +12,11 @@
 
 void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr);
 void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold);
+int do_child_work(int64_t *arr, size_t begin, size_t end, size_t threshold);
+
 
 void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr) {
+  // TODO: implement
   size_t leftIndex = begin;
   size_t rightIndex = mid;
   size_t tempIndex = 0;
@@ -40,9 +43,14 @@ void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr)
     rightIndex++;
     tempIndex++;
   }
+
+  
 }
 
 int cmpvals(const void *p1, const void *p2){
+  // subtraction isn't sufficient 
+  // --> overflow (difference can be less than the minimum of int64_t)
+  //return type is int --> int64 can't be converted to 32
   int64_t left = *(const int64_t*)p1;
   int64_t right = *(const int64_t*)p2;
   
@@ -56,16 +64,18 @@ int cmpvals(const void *p1, const void *p2){
 }
 
 void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
+  // TODO: implement
   if (begin >= end) {
     return;
   }
   size_t numElements = end - begin;
   if ((numElements) <= threshold ) {
-
     
     qsort(arr+begin, numElements, sizeof(int64_t), cmpvals);
     
+    
   } else {
+    //lines 70~110 are fork() implementation
     pid_t pid = fork();
     size_t mid = (begin+end)/2;
     if (pid == -1) {
@@ -74,13 +84,21 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
       fprintf(stderr, "Error: fork failed to start a new process\n");
       exit(1);
     } else if (pid == 0) {
-      merge_sort(arr, begin, mid, threshold);
-      exit(0);
-       // everything past here is now unreachable in the child
-    } else {
-      // parent process
-      merge_sort(arr, mid, end, threshold);
+      int retcode = do_child_work(arr, begin, mid, threshold);
+      exit(retcode);
+      // everything past here is now unreachable in the child
     }
+    pid_t pid1 = fork();
+    if (pid1 == -1) {
+      // fork failed to start a new process
+      // handle the error and exit
+      fprintf(stderr, "Error: fork failed to start a new process\n");
+      exit(1);
+    } else if (pid1 == 0) {
+      int retcode1 = do_child_work(arr, mid, end, threshold);
+      exit(retcode1);
+      // everything past here is now unreachable in the child
+    } 
     
     int wstatus;
     // blocks until the process indentified by pid_to_wait_for completes
@@ -88,16 +106,41 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
     if (actual_pid == -1) {
       // handle waitpid failure
       fprintf(stderr, "Error: waitpid failure\n");
-      exit(1);
+      waitpid(pid1, NULL, 0);
+      exit(1); // exit with 1 is going to terminate the entire process
     }
 
     if (!WIFEXITED(wstatus)) {
       // subprocess crashed, was interrupted, or did not exit normally
       // handle as error
       fprintf(stderr, "Error: subprocess crashed, was interrupted, or did not exit normally\n");
+      waitpid(pid1, NULL, 0);
       exit(1);
     }
     if (WEXITSTATUS(wstatus) != 0) {
+      // subprocess returned a non-zero exit code
+      // if following standard UNIX conventions, this is also an error
+      fprintf(stderr, "Error: subprocess returned a non-zero exit code\n");
+      waitpid(pid1, NULL, 0);
+      exit(1);
+    }
+
+    int wstatus1;
+    // blocks until the process indentified by pid_to_wait_for completes
+    pid_t actual_pid1 = waitpid(pid1, &wstatus1, 0);
+    if (actual_pid1 == -1) {
+      // handle waitpid failure
+      fprintf(stderr, "Error: waitpid failure\n");
+      exit(1); // exit with 1 is going to terminate the entire process
+    }
+
+    if (!WIFEXITED(wstatus1)) {
+      // subprocess crashed, was interrupted, or did not exit normally
+      // handle as error
+      fprintf(stderr, "Error: subprocess crashed, was interrupted, or did not exit normally\n");
+      exit(1);
+    }
+    if (WEXITSTATUS(wstatus1) != 0) {
       // subprocess returned a non-zero exit code
       // if following standard UNIX conventions, this is also an error
       fprintf(stderr, "Error: subprocess returned a non-zero exit code\n");
@@ -105,15 +148,35 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
     }
     // if pid is not 0, we are in the parent process
     // // WARNING, if the child process path can get here, things will quickly break very badly
-  
-    int64_t temparr[numElements];
+    
+
+   
+
+    //malloc size of int64
+    int64_t* temparr;
+    temparr = malloc(sizeof(int64_t)*numElements);
+
     merge(arr, begin, mid, end, temparr);
 
     for (size_t i = begin; i < end; i++) {
       //update original array
       arr[i] = temparr[i-begin];
     }
+    free(temparr);
+
+    
   }
+
+  
+}
+
+
+
+int do_child_work(int64_t *arr, size_t begin, size_t end, size_t threshold) {
+  // this is now in the child process
+  merge_sort(arr, begin, end, threshold);
+  return 0;
+  // TODO: a child process not exiting normally, or exiting with a non-zero exit code?
 }
 
 int main(int argc, char **argv) {
@@ -128,13 +191,13 @@ int main(int argc, char **argv) {
   char *end;
   size_t threshold = (size_t) strtoul(argv[2], &end, 10);
   if (end != argv[2] + strlen(argv[2])) {
-    /* report an error (threshold value is invalid) */;
+    /* TODO: report an error (threshold value is invalid) */;
     fprintf(stderr, "Error: invalid threshold value\n");
     return 1;
   }
     
 
-  // open the file
+  // TODO: open the file
   int fd = open(filename, O_RDWR);
   if (fd < 0) {
     // file couldn't be opened: handle error and exit
@@ -142,7 +205,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // use fstat to determine the size of the file
+  // TODO: use fstat to determine the size of the file
   struct stat statbuf;
   int rc = fstat(fd, &statbuf);
   if (rc != 0) {
@@ -152,7 +215,7 @@ int main(int argc, char **argv) {
   }
   size_t file_size_in_bytes = statbuf.st_size;
 
-  // map the file into memory using mmap
+  // TODO: map the file into memory using mmap
   int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (data == MAP_FAILED) {
     // handle mmap error and exit
@@ -163,11 +226,11 @@ int main(int argc, char **argv) {
   // of the array will silently extend the file, which can rapidly lead to disk space
   // depletion!
 
-  // sort the data!
+  // TODO: sort the data!
   size_t numElements = file_size_in_bytes/sizeof(int64_t);
   merge_sort(data, 0, numElements, threshold);
 
-  // unmap and close the file
+  // TODO: unmap and close the file
   int retVal = munmap(data, file_size_in_bytes);
   if (retVal == -1) {
     fprintf(stderr, "Error: failure to munmap the file data\n");
@@ -179,6 +242,6 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // exit with a 0 exit code if sort was successful
+  // TODO: exit with a 0 exit code if sort was successful
   return 0;
 }
