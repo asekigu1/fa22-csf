5ab46167fef97814a6ee79c9a82a5d1688bc0b1c
aimi <aimilovefuture@gmail.com>
2022-12-10 14:42:54 -0500

    Update room.cpp

--- hw5/room.cpp
+++ hw5/room.cpp
@@ -17,24 +17,25 @@ Room::~Room() {
 
 void Room::add_member(User *user) {
   // TODO: add User to the room
+  Guard g(lock);
   members.insert(user);
 
 }
 
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
+  Guard g(lock);
   members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
-
+  Guard g(lock);
   for (User* user: members){
     if (user->username != sender_username) {
       std::string data = room_name + ":" + sender_username + ":" + message_text;
       Message* message = new Message("delivery", data);
       user->mqueue.enqueue(message);
-      user->user_info->conn_info->send(*user->mqueue.dequeue());
     }
   }
 }
